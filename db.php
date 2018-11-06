be8160e03a3dca3c3712cf2dbdfc857d2017641f
marilson <marilson.ssantos@gmail.com>
2018-11-05 23:24:20 -0200

    shop master

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+
+$connection = mysqli_connect('node173830-meuprojeto.jelasticlw.com.br', 'root', 'KXMbkv79541', 'smartshop');  
+ if(!$connection) {
+     die("Database connection failed");
+ }
+
+ ?>
